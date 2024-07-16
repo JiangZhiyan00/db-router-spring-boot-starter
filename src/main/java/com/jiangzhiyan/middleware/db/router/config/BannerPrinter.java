93236057f95d4a0cc44c0252dfc9b48618677840
JiangZhiyan <jaemusic@foxmail.com>
2024-07-16 14:00:38 +0800

    fix: 在启动后在打印,监听时机应为项目启动后

--- src/main/java/com/jiangzhiyan/middleware/db/router/config/BannerPrinter.java
+++ src/main/java/com/jiangzhiyan/middleware/db/router/config/BannerPrinter.java
@@ -1,7 +1,7 @@
 package com.jiangzhiyan.middleware.db.router.config;
 
 import org.springframework.boot.ResourceBanner;
-import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
@@ -10,14 +10,14 @@ import org.springframework.core.io.Resource;
 /**
  * banner图打印器
  */
-public class BannerPrinter implements ApplicationListener<ApplicationEnvironmentPreparedEvent> {
+public class BannerPrinter implements ApplicationListener<ApplicationStartedEvent> {
 
-    private static final String BANNER_ENABLE = "db-router.banner.enabled";
+    private static final String BANNER_ENABLE = "db-router.banner.enable";
 
     @Override
-    public void onApplicationEvent(ApplicationEnvironmentPreparedEvent event) {
-        Environment environment = event.getEnvironment();
-        String bannerEnable = environment.getProperty(BANNER_ENABLE);
+    public void onApplicationEvent(ApplicationStartedEvent event) {
+        Environment environment = event.getApplicationContext().getEnvironment();
+        String bannerEnable = event.getApplicationContext().getEnvironment().getProperty(BANNER_ENABLE);
 
         //判断 bannerEnable 是否为空或者不为 "false"
         if (!"false".equalsIgnoreCase(bannerEnable)) {
